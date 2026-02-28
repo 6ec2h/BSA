43030fdc4d800779d749b8b87510ebfb7ac98008
6ec2h <besdvah@yandex.ru>
2026-02-28 21:08:54 +0300

    16.02.2026 (3)

--- scripts/vscripts/heroes/hero_crystal_maiden/crystal_maiden_arcane_aura_lua/crystal_maiden_arcane_aura_lua.lua
+++
@@ -1,9 +0,0 @@
-crystal_maiden_arcane_aura_lua = class({})
-LinkLuaModifier( "modifier_crystal_maiden_arcane_aura_lua", "heroes/hero_crystal_maiden/crystal_maiden_arcane_aura_lua/modifier_crystal_maiden_arcane_aura_lua", LUA_MODIFIER_MOTION_NONE )
-LinkLuaModifier( "modifier_crystal_maiden_arcane_aura_lua_effect", "heroes/hero_crystal_maiden/crystal_maiden_arcane_aura_lua/modifier_crystal_maiden_arcane_aura_lua_effect", LUA_MODIFIER_MOTION_NONE )
-
---------------------------------------------------------------------------------
--- Passive Modifier
-function crystal_maiden_arcane_aura_lua:GetIntrinsicModifierName()
-	return "modifier_crystal_maiden_arcane_aura_lua"
-end
--- scripts/vscripts/libraries/base_npc.lua
+++
@@ -1,9 +0,0 @@
-local sheepsName = {
-	["npc_snow"] = true,
-	["npc_snow2"] = true,
-	["npc_snow3"] = true,
-}
-
-function CDOTA_BaseNPC:IsQuestSheep()
-	return sheepsName[self:GetUnitName()] or false
-end
