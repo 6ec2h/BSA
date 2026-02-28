892ef84223d129fee183ed11cb7ccb47d4e871f8
6ec2h <besdvah@yandex.ru>
2026-02-28 22:56:58 +0300

    16.02.2026 (перезалив 2)

--- panorama/layout/custom_game/guild/structures/settingsbitfield.js
+++
@@ -1,6 +0,0 @@
-/**
- * @extends {BitField<typeof SettingsFlags>}
- */
-class SettingsBitField extends BitField {
-	static flags = SettingsFlags
-}
--- scripts/vscripts/heroes/hero_viper/viper_corrosive_skin_lua/viper_corrosive_skin_lua.lua
+++
@@ -1,9 +0,0 @@
-viper_corrosive_skin_lua = class({})
-LinkLuaModifier( "modifier_viper_corrosive_skin_lua", "heroes/hero_viper/viper_corrosive_skin_lua/modifier_viper_corrosive_skin_lua", LUA_MODIFIER_MOTION_NONE )
-LinkLuaModifier( "modifier_viper_corrosive_skin_lua_debuff", "heroes/hero_viper/viper_corrosive_skin_lua/modifier_viper_corrosive_skin_lua_debuff", LUA_MODIFIER_MOTION_NONE )
-
---------------------------------------------------------------------------------
--- Passive Modifier
-function viper_corrosive_skin_lua:GetIntrinsicModifierName()
-	return "modifier_viper_corrosive_skin_lua"
-end
